14a213b4fe0ae911bf4afb5a92c87c587d16a1c2
yitzhak <idashevsky@gmail.com>
2019-05-14 12:05:26 +0300

    add matches script

---
+++ basic-types/with-matches.exs
@@ -0,0 +1,9 @@
+result =
+  with {:ok, file} = File.open("/etc/passwd"),
+       content = IO.read(file, :all),
+       :ok = File.close(file),
+       [_, uid, gid] <- Regex.run(~r/^_xxx:.*?:(\d+):(\d+)/m, content) do
+    "Group: #{gid}, User: #{uid}"
+  end
+
+IO.puts inspect(result)
