102a7add19be5fb474d6e1fd5ace6e1c7ed4192d
yitzhak <idashevsky@gmail.com>
2019-05-20 23:02:15 +0300

    solve ex.4

--- anonymous-functions/exercises.exs
+++ anonymous-functions/exercises.exs
@@ -1,3 +1,11 @@
+# 1. list_concat.([:a, :b], [:c, :d]) => [:a, :b, :c, :d]
+# 2. sum.(1, 2, 3) => 6
+# 3. pair_tuple_to_list.( { 1234, 5678 } ) => [ 1234, 5678 ]
+
+list_concat = fn (arr1, arr2) -> arr1 ++ arr2 end
+sum = fn (a, b, c) -> a + b + c end
+pair_tuple_to_list = fn ({a, b}) -> [a, b] end
+
 # Exercise: Functions-2
 # Write a function that takes three arguments.
 # If the first two are zero, return “FizzBuzz.”
@@ -19,3 +27,6 @@ end
 # You should get “Buzz, 11, Fizz, 13, 14, FizzBuzz, 16.”
 
 func_2 = fn (n) -> func_1.(rem(n, 3), rem(n, 5), n) end
+
+# Exercise: Functions-4
+prefix = fn first -> (fn second -> "#{first} #{second}"  end)  end
