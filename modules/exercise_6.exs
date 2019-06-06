16232648ea073267a711b0ad6684f71b4723ca30
Yitzhak Dashevsky <yitzhakd@compiralabs.com>
2019-06-06 17:34:59 +0300

    ex. #6

--- modules/exercise_6.exs
+++ modules/exercise_6.exs
@@ -1,5 +1,13 @@
 defmodule Chop do
-  def guess(n, range) do
-
+  def guess(n, n) when is_number(n) do
+    n
+  end
+  def guess(n, a..b) do
+    candidate = div(b, 2)
+    IO.puts("Is it #{candidate}")
+    guess(n, candidate)
+    IO.puts(b)
+    IO.puts(n)
+    IO.puts(a)
   end
 end
