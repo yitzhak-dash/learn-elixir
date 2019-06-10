240677150a3ed9738e3bb04469c67d93bdc6cd8b
yitzhak <idashevsky@gmail.com>
2019-06-10 22:56:33 +0300

    ex.6

--- modules/exercise_6.exs
+++ modules/exercise_6.exs
@@ -1,13 +1,21 @@
 defmodule Chop do
-  def guess(n, n) when is_number(n) do
-    n
+
+  def _guess(n, a..b) when n not in a..b do
+    ""
   end
-  def guess(n, a..b) do
-    candidate = div(b, 2)
-    IO.puts("Is it #{candidate}")
-    guess(n, candidate)
-    IO.puts(b)
+
+  def _guess(n, n..b) do
     IO.puts(n)
-    IO.puts(a)
+  end
+
+  def _guess(n, a..b) when n in a..b do
+    guess(n, a..b)
+  end
+
+  def guess(n, a..b) do
+    candidate = a + div(b - a, 2)
+    IO.puts("Is it #{candidate}")
+    _guess(n, a..(candidate - 1))
+    _guess(n, candidate..b)
   end
 end
