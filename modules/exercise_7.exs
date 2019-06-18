ef26ed73107b632e9772bc613642c5c9b6b01e56
Yitzhak Dashevsky <yitzhakd@compiralabs.com>
2019-06-18 13:50:45 +0300

    ex. #7

---
+++ modules/exercise_7.exs
@@ -0,0 +1,16 @@
+# 1.
+IO.puts(:io_lib.format("~.2f", [546.6578]))
+
+# 2
+IO.puts(System.get_env())
+
+# 3
+IO.puts(Path.extname("./exercise_7.exs"))
+
+# 4
+IO.puts(System.cwd())
+
+#5
+
+#6
+IO.puts(System.cmd("whoami", []))
