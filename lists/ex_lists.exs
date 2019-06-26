7e2fe031537f6f7c11413f4802c69e0d5d33edad
yitzhak <idashevsky@gmail.com>
2019-06-26 23:31:51 +0300

    ex #1, refactoring

--- lists/ex_lists.exs
+++ lists/ex_lists.exs
@@ -1,3 +1,5 @@
 defmodule ExerciseList_1 do
-  def mapsum(list, func), do: MyList.reduce(MyList.map(list, func), 0, &(&1 + &2))
+  def mapsum(list, func), do: mapsum(list, 0, func)
+  defp mapsum([], val, _func), do: val
+  defp mapsum([head | tail], val, func), do: mapsum(tail, func.(head) + val, func)
 end
