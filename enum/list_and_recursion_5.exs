383a1d4199a5a98a36051abe048dde1c16ba10a1
Yitzhak Dashevsky <yitzhakd@compiralabs.com>
2020-01-14 16:14:31 +0200

    solve ex.5

--- enum/list_and_recursion_5.exs
+++ enum/list_and_recursion_5.exs
@@ -1,11 +1,23 @@
 defmodule CustomEnum do
-#  ex.5
+  #  ex.5
+
+  #all?
   def all?([], func), do: true
   def all?([head | tail], func) do
     if func.(head) do
       all?(tail, func)
     else
       false
+    end
+  end
+
+  # filter
+  def filter([], func?), do: []
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
     end
   end
 end
