627111ae55333f269175f9d31a5dcba03158ebaf
Yitzhak Dashevsky <yitzhakd@compiralabs.com>
2020-07-05 15:46:44 +0300

    rename params

--- enum/list_and_recursion_5.exs
+++ enum/list_and_recursion_5.exs
@@ -42,11 +42,24 @@ defmodule CustomEnum do
   # TODO: support negative amount(the amount of last values will be taken)
   # Takes the first amount items from the enumerable.
   def take([], _), do: []
+
+  def take(list, amount) when amount < 0 do
+    take(list, length(list) + amount, amount * (-1))
+  end
+
   def take([head | tail], amount) do
     if amount == 0 do
       []
     else
       [head] ++ take(tail, amount - 1)
+    end
+  end
+
+  def take([head | tail], from, amount) do
+    if from > 0 do
+      take(tail, from - 1, amount)
+    else
+      take([head | tail], amount)
     end
   end
 
